80a7fdb667d7b712351f6d7dab3ac63308f27658
SFC <115345487+Sheifc@users.noreply.github.com>
2023-07-27 15:57:33 +0200

    Add files via upload

---
+++ rush02/com.c
@@ -0,0 +1,15 @@
+#include "init.h"
+#include "com.h"
+
+int start_com (void)
+{
+	int a;
+
+	a = 22;
+	return 0;
+}
+
+int stop_com(void)
+{
+	return (0);
+}
---
+++ rush02/com.h
@@ -0,0 +1,10 @@
+#ifndef _H_COM
+#define _H_COM
+
+#define COM_VALUE 100
+#define NEW_VALUE 2
+int	start_com(void);
+int stop_com(void);
+int send_message(char *msg);
+
+#endif
