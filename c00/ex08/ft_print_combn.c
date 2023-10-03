37698c3186369f8a01d3ae249cb7c49559a65e35
SFC <115345487+Sheifc@users.noreply.github.com>
2023-10-03 09:43:58 +0200

    Update ft_print_combn.c

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -12,11 +12,49 @@
 
 #include <unistd.h>
 
-void	ft_putchar(n)
+void	ft_putchar(char c)
 {
-	write(1, &n, 1);
+	write(1, &c, 1);
 }
 
+void	ft_print_combn_recursive(int n, int start, int num, int arr[])
+{
+	int	i;
+
+	i = 0;
+	if (n == 0)
+	{
+		while (i < num)
+		{
+			ft_putchar(arr[i] + '0');
+			i++;
+		}
+		if (arr[0] != 10 - num)
+		{
+			ft_putchar(',');
+			ft_putchar(' ');
+		}
+		return ;
+	}
+	i = start;
+	while (i <= 9)
+	{
+		arr[num] = i; 
+		ft_print_combn_recursive(n - 1, i + 1, num + 1, arr);
+		i++;
+	}
+}
+
+// Add the digit 'i' to the 'arr' array at index 'num'
 void	ft_print_combn(int n)
 {
+	int	arr[10];
+
+	ft_print_combn_recursive(n, 0, 0, arr);
 }
+/*
+int main(void)
+{
+    ft_print_combn(2);
+    return (0);
+}*/
